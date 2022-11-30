b6ce2a067d36b06a721068c2e8ab43368371ec2d
dar-jeeling <o_o___t@pusan.ac.kr>
2022-12-01 04:51:39 +0900

    update: meta 데이터 없는 아이디 공백으로 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -24,7 +24,7 @@ module.exports = {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'dar-jeeling',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
