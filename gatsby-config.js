88c7512a19827d987914e73f41f2bd5f9f12ef0c
dar-jeeling <o_o___t@pusan.ac.kr>
2022-12-01 12:15:31 +0900

    design: 모바일 헤더 진한색으로 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         short_name: metaConfig.title,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#8bc6ec`,
+        theme_color: `#90AEE6`,
         display: `minimal-ui`,
         icon: metaConfig.icon,
       },
