a5b3d200c8b4e59afb340fe402b964a8f54ec49b
dar-jeeling <o_o___t@pusan.ac.kr>
2022-12-01 21:46:14 +0900

    feat: 게시글 썸네일에 날짜 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
+import dayjs from 'dayjs'
 
 import './index.scss'
 
-export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
-)
+export const ThumbnailItem = ({ node }) => {
+  const date = new dayjs(node.frontmatter.date).format('YY년 MM월 DD일')
+
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div key={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+        <p
+          class="thumbnail-item"
+          dangerouslySetInnerHTML={{ __html: node.excerpt }}
+        />
+        <span class="thumbnail-item__date">{date}</span>
+      </div>
+    </Link>
+  )
+}
