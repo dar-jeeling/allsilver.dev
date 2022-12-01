d0984cd3c181e1202c10f1948d296e5172a22e4e
dar-jeeling <o_o___t@pusan.ac.kr>
2022-12-01 20:48:44 +0900

    chore: public/images alias 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/static/images`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
